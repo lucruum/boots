a2bb4bf2e2985e69ba2411fc317eae14807aa0a6
lucruum <naoh432@mail.ru>
2022-11-07 16:44:51 +0300

    feat(evol): отображение индекса текущего коммита
    $ python -m evol 17
     _____________
    /_   \______  \
     |   |   /    /
     |   |  /    /
     |___| /____/
    
    commit 54240b4811b66dc7d5c2738b91994c21023f0e9e (HEAD)
    Author: Kenneth Reitz <me@kennethreitz.com>
    Date:   Sun Feb 13 16:24:14 2011 -0500
    
        core update
    
     requests/core.py | 29 +++++++++++++++++++++++++----
     1 file changed, 25 insertions(+), 4 deletions(-)
    Unstaged changes after reset:
    M       requests/core.py

--- evol.py
+++ evol.py
@@ -2,6 +2,53 @@ import subprocess
 import sys
 
 
+glyph_width = 10
+glyph_height = 6
+glyphs = [
+    ["_______   ", "\\   _  \\  ", "/  /_\\  \\ ", "\\  \\_/   \\", " \\_____  /", "       \\/ "],
+    [" ____     ", "/_   |    ", " |   |    ", " |   |    ", " |___|    ", "          "],
+    ["________  ", "\\_____  \\ ", " /  ____/ ", "/       \\ ", "\\_______ \\", "        \\/"],
+    ["________  ", "\\_____  \\ ", "  _(__  < ", " /       \\", "/______  /", "       \\/ "],
+    ["   _____  ", "  /  |  | ", " /   |  |_", "/    ^   /", "\\____   | ", "     |__| "],
+    [" .________", " |   ____/", " |____  \\ ", " /       \\", "/______  /", "       \\/ "],
+    ["  ________", " /  _____/", "/   __  \\ ", "\\  |__\\  \\", " \\_____  /", "       \\/ "],
+    ["_________ ", "\\______  \\", "    /    /", "   /    / ", "  /____/  ", "          "],
+    ["  ______  ", " /  __  \\ ", " >      < ", "/   --   \\", "\\______  /", "       \\/ "],
+    [" ________ ", "/   __   \\", "\\____    /", "   /    / ", "  /____/  ", "          "],
+]
+
+offsets = [
+    [1, 2, 0, 1, 0, 1, 1, 3, 0, 2],
+    [5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
+    [2, 1, 1, 0, 1, 0, 2, 2, 1, 1],
+    [1, 1, 0, 1, 0, 1, 1, 2, 0, 1],
+    [0, 2, 1, 2, 1, 2, 0, 2, 1, 1],
+    [0, 1, 0, 0, 0, 1, 1, 0, 0, 1],
+    [0, 1, 0, 0, 0, 1, 1, 0, 0, 1],
+    [1, 0, 1, 1, 2, 2, 1, 1, 1, 0],
+    [1, 1, 0, 1, 0, 1, 1, 2, 0, 1],
+    [1, 0, 1, 1, 2, 2, 1, 1, 1, 0],
+]
+
+
+def render(x):
+    digits = [int(it) for it in str(x)]
+
+    positions = [0]
+    for i in range(len(digits) - 1):
+        positions.append(positions[-1] + glyph_width - offsets[digits[i]][digits[i + 1]])
+
+    art = [[" " for _ in range(positions[-1] + glyph_width)] for _ in range(glyph_height)]
+
+    for i, it in enumerate(digits):
+        for y in range(glyph_height):
+            for x in range(glyph_width):
+                if glyphs[it][y][x] != " " and art[y][positions[i] + x] in (" ", "|", "_"):
+                    art[y][positions[i] + x] = glyphs[it][y][x]
+
+    return "\n".join("".join(it) for it in art)
+
+
 if len(sys.argv) == 2 and sys.argv[1] == "rollback":
     subprocess.run(
         "git reset --hard &"
@@ -30,7 +77,9 @@ subprocess.run(
 
 subprocess.run(f"git rev-list HEAD --reverse | sed '{n}q;d' | xargs git checkout", shell=True)
 
+subprocess.run("cls", shell=True)
+print(render(n))
 subprocess.run(
-    "cls & git show --stat & git reset HEAD~1 --quiet --soft & git reset & code . --reuse-window",
+    "git show --stat & git reset HEAD~1 --quiet --soft & git reset & code . --reuse-window",
     shell=True,
 )
