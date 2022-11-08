4b95e5f9f59a5ab5a9a30a776f4c55317c67a358
lucruum <naoh432@mail.ru>
2022-11-08 23:30:47 +0300

    feat(gitconfig): начальная версия

--- strap.sh
+++ strap.sh
@@ -1,3 +1,5 @@
+GIT_GLOBAL_CONFIG_PATH=`git -c core.editor=echo config --global --edit | sed "s/hint: Waiting for your editor to close the file... //"`
 PYTHON_SITE_PACKAGES_PATH=`python -m site --user-site`
 
+cp ".gitconfig" "$GIT_GLOBAL_CONFIG_PATH" --force
 find -name "*.py" -exec cp {} "$PYTHON_SITE_PACKAGES_PATH" --force \;
