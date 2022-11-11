6ab0d1baa4d35f34f3c10b971a4831998068e85e
lucruum <naoh432@mail.ru>
2022-11-11 05:08:16 +0300

    fix(evol): поправил эволюцию параллельных ветвей
    `git rev-list HEAD | wc --lines` возвращает порядковый номер HEAD'а, не беря
    во внимание коммиты из параллельной ветки
    
    $ python -m evol 11
    …
    $ git log master --decorate --graph --oneline
    …
    *   81dd6dc added mp3 conversion
    |\
    | * 65f94b9 Delete YTMusic.py
    * | b5ff234 (HEAD) added mp3 conversion
    * | 4275081 mp3 conversion
    |/
    * eb4471d Update README.md
    * 20b2709 Update README.md
    * 007d7b2 Update README.md
    * 03d15ec YTMusic.py
    * af9371a Delete YTMusic.py
    * 47dd6a3 README.md
    * b473f8f Brief info about Project
    * c8d3e72 YTMusic
    $ diff <(git rev-list HEAD --abbrev-commit) <(git rev-list master --abbrev-commit | tail --lines=11) --side-by-side
    b5ff234       b5ff234
    4275081       4275081
                > 65f94b9
    eb4471d       eb4471d
    20b2709       20b2709
    007d7b2       007d7b2
    03d15ec       03d15ec
    af9371a       af9371a
    47dd6a3       47dd6a3
    b473f8f       b473f8f
    c8d3e72       c8d3e72

--- evol.py
+++ evol.py
@@ -49,16 +49,31 @@ def render(x):
     return "\n".join("".join(it) for it in art)
 
 
+default_branch = subprocess.check_output(
+    "git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@'",
+    shell=True,
+    text=True,
+).strip()
+
 if len(sys.argv) == 1:
     if subprocess.run("git symbolic-ref HEAD --quiet", stdout=subprocess.DEVNULL).returncode == 0:
         n = 1
     else:
-        n = int(subprocess.check_output("git rev-list HEAD | wc --lines", shell=True)) + 1
+        head_hash = subprocess.check_output("git rev-parse HEAD", shell=True, text=True).strip()
+        n = (
+            int(
+                subprocess.check_output(
+                    f"git rev-list {default_branch} --reverse | grep {head_hash} --line-number | cut --fields=1 --delimiter=':'",
+                    shell=True,
+                )
+            )
+            + 1
+        )
 else:
     n = int(sys.argv[1])
 
 subprocess.run("cls & git rollback", shell=True)
-subprocess.run(f"git rev-list HEAD --reverse | sed '{n}q;d' | xargs git checkout", shell=True)
+subprocess.run(f"git rev-list {default_branch} --reverse | sed '{n}q;d' | xargs git checkout", shell=True)
 subprocess.run("cls", shell=True)
 print(render(n))
 subprocess.run("git show --stat & code . --reuse-window", shell=True)
