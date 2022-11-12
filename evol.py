00b959aea818882f60c8d682f30299d257fe0e85
lucruum <naoh432@mail.ru>
2022-11-13 00:53:41 +0300

    feat(evol,graffiti): вынес graffiti в отдельный модуль

--- evol.py
+++ evol.py
@@ -1,52 +1,7 @@
 import subprocess
 import sys
 
-
-glyph_width = 10
-glyph_height = 6
-glyphs = [
-    ["_______   ", "\\   _  \\  ", "/  /_\\  \\ ", "\\  \\_/   \\", " \\_____  /", "       \\/ "],
-    [" ____     ", "/_   |    ", " |   |    ", " |   |    ", " |___|    ", "          "],
-    ["________  ", "\\_____  \\ ", " /  ____/ ", "/       \\ ", "\\_______ \\", "        \\/"],
-    ["________  ", "\\_____  \\ ", "  _(__  < ", " /       \\", "/______  /", "       \\/ "],
-    ["   _____  ", "  /  |  | ", " /   |  |_", "/    ^   /", "\\____   | ", "     |__| "],
-    [" .________", " |   ____/", " |____  \\ ", " /       \\", "/______  /", "       \\/ "],
-    ["  ________", " /  _____/", "/   __  \\ ", "\\  |__\\  \\", " \\_____  /", "       \\/ "],
-    ["_________ ", "\\______  \\", "    /    /", "   /    / ", "  /____/  ", "          "],
-    ["  ______  ", " /  __  \\ ", " >      < ", "/   --   \\", "\\______  /", "       \\/ "],
-    [" ________ ", "/   __   \\", "\\____    /", "   /    / ", "  /____/  ", "          "],
-]
-
-offsets = [
-    [1, 2, 0, 1, 0, 1, 1, 3, 0, 2],
-    [5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
-    [2, 1, 1, 0, 1, 0, 2, 2, 1, 1],
-    [1, 1, 0, 1, 0, 1, 1, 2, 0, 1],
-    [0, 2, 1, 2, 1, 2, 0, 2, 1, 1],
-    [0, 1, 0, 0, 0, 1, 1, 0, 0, 1],
-    [0, 1, 0, 0, 0, 1, 1, 0, 0, 1],
-    [1, 0, 1, 1, 2, 2, 1, 1, 1, 0],
-    [1, 1, 0, 1, 0, 1, 1, 2, 0, 1],
-    [1, 0, 1, 1, 2, 2, 1, 1, 1, 0],
-]
-
-
-def render(x):
-    digits = [int(it) for it in str(x)]
-
-    positions = [0]
-    for i in range(len(digits) - 1):
-        positions.append(positions[-1] + glyph_width - offsets[digits[i]][digits[i + 1]])
-
-    art = [[" " for _ in range(positions[-1] + glyph_width)] for _ in range(glyph_height)]
-
-    for i, it in enumerate(digits):
-        for y in range(glyph_height):
-            for x in range(glyph_width):
-                if glyphs[it][y][x] != " " and art[y][positions[i] + x] in (" ", "|", "_"):
-                    art[y][positions[i] + x] = glyphs[it][y][x]
-
-    return "\n".join("".join(it) for it in art)
+import graffiti
 
 
 default_branch = subprocess.check_output(
@@ -80,6 +35,6 @@ else:
 subprocess.run("cls & git rollback", shell=True)
 subprocess.run(f"git rev-list {default_branch} --reverse | sed '{n}q;d' | xargs git checkout", shell=True)
 subprocess.run("cls", shell=True)
-print(render(n))
+print(graffiti.render(str(n)))
 subprocess.run("git show --stat & code . --reuse-window", shell=True)
 subprocess.run("git revert HEAD --no-edit & git revert HEAD --no-commit & git reset", shell=True)
