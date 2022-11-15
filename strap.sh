29fa09673a91d5076a0e399decfc0f8eedce4cdc
lucruum <naoh432@mail.ru>
2022-11-16 00:57:31 +0300

    chore: поправил strap.sh
    Python создаёт папку site-packages по мере необходимости, поэтому "подковка"
    только что установленного интерпретатора может обернуться ошибкой:
    
    > bash strap.sh
    cp: cannot stat '…\site-packages': Not a directory

--- strap.sh
+++ strap.sh
@@ -1,5 +1,7 @@
 GIT_GLOBAL_CONFIG_PATH=`git -c core.editor=echo config --global --edit | sed "s/hint: Waiting for your editor to close the file... //"`
 PYTHON_SITE_PACKAGES_PATH=`python -m site --user-site`
 
+mkdir "$PYTHON_SITE_PACKAGES_PATH" --parents
+
 cp ".gitconfig" "$GIT_GLOBAL_CONFIG_PATH" --force
 find -name "*.py" -exec cp {} "$PYTHON_SITE_PACKAGES_PATH" --force \;
